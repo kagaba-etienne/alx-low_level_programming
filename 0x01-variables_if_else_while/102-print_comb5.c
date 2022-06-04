3002636f7994a8eb6c2863e67a82ec915d34c25e
kagaba--etienne <kagabaetienne04@gmail.com>
2022-06-03 18:26:29 -0700

    advanced combinations

---
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -0,0 +1,39 @@
+#include<stdio.h>
+
+/**
+ * main - entry point
+ *
+ * Return: Always zero
+ */
+int main(void)
+{
+	int i;
+	int j;
+	int k;
+	int l;
+
+	for (i = 48; i < 58; i++)
+	{
+		for (j = 48; j < 58; j++)
+		{
+			for (k = i; k < 58; k++)
+			{
+				for (l = j + 1; l < 58; l++)
+				{
+					putchar(i);
+					putchar(j);
+					putchar(' ');
+					putchar(k);
+					putchar(l);
+					if (i != 57 || j != 56)
+					{
+						putchar(',');
+						putchar(' ');
+					}
+				}
+			}
+		}
+	}
+	putchar('\n');
+	return (0);
+}
