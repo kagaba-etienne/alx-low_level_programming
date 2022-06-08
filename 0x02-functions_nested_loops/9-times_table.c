01be05ba3cb982325352fde2a997bef3e17571e0
kagaba-etienne <kagabaetienne04@gmail.com>
2022-06-08 20:58:44 +0200

    no9

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -9,37 +9,27 @@ void times_table(void)
 	int a;
 	int b;
 	int c;
-	int d;
-	int e;
-	int f;
-	int g;
-	int h;
-	int i;
-	int j;
-	int k;
 
-	a = 0;
-	b = 1;
-	c = 2;
-	d = 3;
-	e = 4;
-	f = 5;
-	g = 6;
-	h = 7;
-	i = 8;
-	j = 9;
-	for (k = 0; k < 10; k++)
+	for (a = 0; a < 10; a++)
 	{
-		_putchar(a * k + '0');
-		_putchar(b * k + '0');
-		_putchar(c * k + '0');
-		_putchar(d * k + '0');
-		_putchar(e * k + '0');
-		_putchar(f * k + '0');
-		_putchar(g * k + '0');
-		_putchar(h * k + '0');
-		_putchar(i * k + '0');
-		_putchar(j * k + '0');
-		_putchar('\n');
+		for (b = 0; b < 10; b++)
+		{
+			c = a * b;
+			_putchar(c + '0');
+			if ((c + a) < 10 && b != 9)
+			{
+				_putchar(',');
+				_putchar(' ');
+				_putchar(' ');
+			}
+			else if (b != 9)
+			{
+				_putchar(' ');
+			}
+			else
+			{
+				_putchar('\n');
+			}
+		}
 	}
 }
