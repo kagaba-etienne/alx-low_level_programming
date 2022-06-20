537ef8d6a68d0c0fe098cea8c846822b0fc85849
kagaba-etienne <kagabaetienne04@gmail.com>
2022-06-20 21:48:38 +0200

    first pointers

--- 0x07-pointers_arrays_strings/1-memcpy.c
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -1,10 +1,11 @@
 #include "main.h"
 /**
- * _memcpy - function copies n bytes from memory area src to 
+ * _memcpy - function copies n bytes from memory area src to
  * memory area dest
  * @dest: destination address
  * @src: source address
  * @n: number of bytes
+ * Return: pointer
  */
 char *_memcpy(char *dest, char *src, unsigned int n)
 {
